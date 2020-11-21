77c4ef1ed54bb745a7bba8d8b2e3430de14caec1
Jiyo <ji.yyou@gmail.com>
2020-11-21 16:05:39 -0500

    script.js finished for toggle questions

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const items = document.querySelectorAll('.card__item');
 
-console.log(items);
+const expandFaq = () => {
+	for (let i = 1; i <= items.length; i++) {
+		let item = items[i - 1];
 
-// items.addEventListener('click', e => {
-// 	console.log(e);
-// 	console.log('hello');
-// })
+		item.addEventListener('click', e => {
+			let answer = document.querySelector(`#answer${i}`);
+			let question = document.querySelector(`#q${i}`);
+			let arrow = document.querySelector(`#arrow${i}`);
+			console.log(answer.style.display);	
+			if (answer.style.display === 'block') {
+				answer.style.display = 'none';
+				question.style.fontWeight = 'normal';
+				arrow.style.transform = 'rotate(0deg)';	
+				// answer.style.display = 'block';
+				// question.style.fontWeight = 'bold';
+				// arrow.style.transform = 'rotate(180deg)';
+			}
+			else {
+				answer.style.display = 'block';
+				question.style.fontWeight = 'bold';
+				arrow.style.transform = 'rotate(180deg)';
+				// answer.style.display = 'none';
+				// question.style.fontWeight = 'normal';
+				// arrow.style.transform = 'rotate(180deg)';	
+			}
+		}) 
+	}	
+}
 
-items.forEach(item => {
-	item.addEventListener('click', e => {
-		console.log(e);
-	})
-})
+expandFaq();
